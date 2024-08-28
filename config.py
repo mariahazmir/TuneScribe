066d76dff4c7a7a34bd8e08b1a93e8a1738bc0ca
Mariah <100682739+mariahazmir@users.noreply.github.com>
2024-08-28 19:20:09 +0800

    Delete config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-# Spotify API credentials
-CLIENT_ID = 'enter client id'
-CLIENT_SECRET = 'enter client secret'
-REDIRECT_URI = 'enter redirect uri'
-SCOPES = 'playlist-modify-private playlist-modify-public'
